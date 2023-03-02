863886e75971d70673e96b3d5f70f362a24a2fce
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-02 09:45:36 +0530

    Modules Created

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -9,6 +11,18 @@ const PORT = process.env.PORT || 3000
 
 // log request
 app.use(morgan('tiny'));
+
+// parse request to body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// set view engine
+app.set('view engine', 'ejs')
+// app.set('view',path.resolve(__dirname,"views/ejs"))
+
+// load assets
+app.use('/css',express.static(path.resolve(__dirname, 'assets/css')))
+app.use('/img',express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js',express.static(path.resolve(__dirname, 'assets/js')))
 
 app.get('/', (req, res) => {
     res.send("Crud Application")
