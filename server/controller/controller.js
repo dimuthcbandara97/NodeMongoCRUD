6d9c06ee3a99f0e1cb7966a831d2b95f47fbd186
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-02 16:16:06 +0530

    API Routes added

---
+++ server/controller/controller.js
@@ -0,0 +1,22 @@
+var Userdb = require('../model/model')
+
+// create and save new user
+
+exports.create = (req, res) => {
+
+}
+
+// retreive and return all users
+exports.find = (req, res) => {
+     
+}
+
+// update a new identifed user by user id
+exports.update = (req, res) => {
+
+}
+
+// delete a user by user id
+exports.delete = (req, res) => {
+
+}
