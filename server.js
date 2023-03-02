519f4a68e027c9b47cb74e9950b704f1eb84c9cd
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-02 15:49:55 +0530

    MongoDB Connected

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const connectDB = require('./server/database/connection')
+
 const app = express()
 
 dotenv.config({path:'config.env'})
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 3000
 
 // log request
 app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB()
 
 // parse request to body parser
 app.use(bodyParser.urlencoded({ extended: true }))
