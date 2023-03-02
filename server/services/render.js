0c955cbcafe9c68f7d08f48b80749d5ad4adef42
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-02 18:55:18 +0530

    Using API data loading

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,17 @@
 
+const axios = require('axios');
+
 exports.homeRoutes = (req,res) => {
-    res.render('index')
+    // Make a get request top /api/users
+    axios.get('http://localhost:3000/api/users')
+    .then(function (response) {
+        // console.log(response.data);
+        res.render('index',{users: response.data});
+    }).catch(err=>{
+
+    })
+    
+    
 }
 
 exports.add_user = (req, res) => {
