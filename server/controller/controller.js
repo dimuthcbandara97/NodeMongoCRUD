81f6b9baef8911973e45a54851fa5d4d1f004ed5
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-02 16:44:46 +0530

    Create Method added (MongoDB)

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,7 +3,32 @@ var Userdb = require('../model/model')
 // create and save new user
 
 exports.create = (req, res) => {
+    // validate the request
+    if(!req.body){
+        res.status(400).send({
+            message: "Content can not be empty!"
+        })
+        return 
+    }
 
+    // new user
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    // save user in the database
+    user.save(user)
+      .then(data => {
+            res.send(data)
+        })
+      .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the user."
+            })
+        })
 }
 
 // retreive and return all users
