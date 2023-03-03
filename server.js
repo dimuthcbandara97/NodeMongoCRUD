ca2551b9468c9365d39fbf1f6d8c4ac3432ca71c
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-03 08:01:33 +0530

    Security Hotspots Reviewed and Fixed

--- server.js
+++ server.js
@@ -4,34 +4,41 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+
 const connectDB = require('./server/database/connection')
 
-const app = express()
+const app1 = express()
+app1.disable("x-powered-by");
+
+// helmet security configuration
+let helmet = require("helmet");
+let app2 = express(); // Compliant
+app2.use(helmet.hidePoweredBy());
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 3000
 
 // log request
-app.use(morgan('tiny'));
+app2.use(morgan('tiny'));
 
 //mongodb connection
 connectDB()
 
 // parse request to body parser
-app.use(bodyParser.urlencoded({ extended: true }))
+app2.use(bodyParser.urlencoded({ extended: true }))
 
 // set view engine
-app.set('view engine', 'ejs')
+app2.set('view engine', 'ejs')
 // app.set('view',path.resolve(__dirname,"views/ejs"))
 
 // load assets
-app.use('/css',express.static(path.resolve(__dirname, 'assets/css')))
-app.use('/img',express.static(path.resolve(__dirname, 'assets/img')))
-app.use('/js',express.static(path.resolve(__dirname, 'assets/js')))
+app2.use('/css',express.static(path.resolve(__dirname, 'assets/css')))
+app2.use('/img',express.static(path.resolve(__dirname, 'assets/img')))
+app2.use('/js',express.static(path.resolve(__dirname, 'assets/js')))
 
 // loaed routers
-app.use('/',require('./server/routes/router'))
+app2.use('/',require('./server/routes/router'))
 
-app.listen(PORT, () => {
+app2.listen(PORT, () => {
     console.log("Server is running on port ")
 })
