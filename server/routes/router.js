ca99e576dec91a378edb23680639469ae5481f78
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-09 13:26:15 +0530

    Nutrition Feature Added

--- server/routes/router.js
+++ server/routes/router.js
@@ -28,10 +28,16 @@ route.get('/add-user',services.add_user)
  */
 route.get('/update-user',services.update_user)
 
-// API routes
+// API routes for users
 route.post('/api/users',controller.create)
 route.get('/api/users',controller.find)
 route.put('/api/users/:id',controller.update)
 route.delete('/api/users/:id',controller.delete)
+
+// API routes for Nutrition data
+route.post('/nutrition/users',controller.create)
+route.get('/nutrition/users',controller.find)
+route.put('/api/nutrition/:id',controller.update)
+route.delete('/api/nutrition/:id',controller.delete)
 
 module.exports = route
